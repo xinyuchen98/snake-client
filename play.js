eef874c972e1dabd79274d62708c2215629dd597
Xinyu Chen <xinyuchen98@gmail.com>
2022-01-20 06:34:04 +0000

    Connection module

--- play.js
+++ play.js
@@ -1,21 +1,3 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host:"localhost", // IP address here,
-    port:50541 // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-};
-
+const {connect} = require("./client");
 console.log("Connecting ...");
 connect();
