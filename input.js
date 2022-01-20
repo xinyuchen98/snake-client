97b2027a9b5b0f076e1a1ddc99d099e59834d7c0
Xinyu Chen <xinyuchen98@gmail.com>
2022-01-20 07:27:59 +0000

    WASD Movements

--- input.js
+++ input.js
@@ -1,3 +1,6 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
 
 const handleUserInput = function (key) {
@@ -6,9 +9,22 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
 };
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
--- play.js
+++ play.js
@@ -1,5 +1,4 @@
 const {connect} = require("./client");
 const {setupInput} = require("./input");
 console.log("Connecting ...");
-connect();
-setupInput();
+setupInput(connect());
